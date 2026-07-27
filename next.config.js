958bf72eab3d5363b338064b43be9c04dbc8856d
jonnathanypg <jonnathan.ypg@gmail.com>
2026-07-27 10:18:36 -0500

    fix: match Hostinger auto-detect deployment config by removing output standalone

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // 'standalone' paquetiza todo para deployment en Hostinger Node.js
-  // Permite que el proceso se inicie con: node .next/standalone/server.js
-  output: 'standalone',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
+
